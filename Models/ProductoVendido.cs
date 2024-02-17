df4eea6a41ad014a3ed07faf51b07740136091cc
lautaro-iriazabal <lautaroiriazabal@gmail.com>
2024-02-17 16:05:19 -0300

    Models Producto y prod vendido

--- Models/ProductoVendido.cs
+++ Models/ProductoVendido.cs
@@ -1,6 +1,35 @@
 ﻿namespace WebApiLautaroIriazabal.Models
 {
-    public class ProductoVendido
+    public partial class ProductoVendido
     {
+        // Identificador único del producto vendido
+        public int Id { get; set; }
+
+        // Cantidad de stock del producto vendido
+        public int Stock
+        {
+            get => Stock;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("El stock no puede ser negativo.");
+                }
+                Stock = value;
+            }
+        }
+
+        // Identificador del producto asociado a este producto vendido
+        public int IdProducto { get; set; }
+
+        // Identificador de la venta asociada a este producto vendido
+        public int IdVenta { get; set; }
+
+        // Navegación a la entidad Producto asociada a este producto vendido
+        public virtual Producto IdProductoNavigation { get; set; } = null!;
+
+        // Navegación a la entidad Venta asociada a este producto vendido
+        public virtual Venta IdVentaNavigation { get; set; } = null!;
     }
+
 }
