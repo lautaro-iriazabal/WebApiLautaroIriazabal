d08f376b352a7d825f6a1476cbf0f8f112de4725
lautaro-iriazabal <lautaroiriazabal@gmail.com>
2024-02-17 15:47:18 -0300

    Producto Vendido controllers

---
+++ Controllers/ProductoVendidoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiLautaroIriazabal.Controllers
+{
+    // La anotación ApiController indica que esta clase es un controlador de API.
+    [ApiController]
+    // La anotación Route define la ruta base para las acciones de este controlador.
+    [Route("api/[controller]")]
+    public class ProductoVendidoController : Controller
+    {
+        // Variable privada para almacenar los datos de los productos vendidos.
+        private ProductoVendidoData _productoVendidoData;
+
+        // Constructor que recibe los datos de los productos vendidos como parámetro.
+        public ProductoVendidoController(ProductoVendidoData productoVendidoData)
+        {
+            // Verifica que los datos de los productos vendidos no sean nulos antes de asignarlos.
+            this._productoVendidoData = productoVendidoData ?? throw new ArgumentNullException(nameof(productoVendidoData));
+        }
+
+    }
+
+}
