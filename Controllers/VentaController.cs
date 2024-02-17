3cc030e962f1dfc1603dc65183f27eb3aef03499
lautaro-iriazabal <lautaroiriazabal@gmail.com>
2024-02-17 15:55:10 -0300

    venta controllers

---
+++ Controllers/VentaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiLautaroIriazabal.Controllers
+{
+    // La anotación ApiController indica que esta clase es un controlador de API.
+    [ApiController]
+    // La anotación Route define la ruta base para las acciones de este controlador.
+    [Route("api/[controller]")]
+    public class VentaController : Controller
+    {
+        // Variable privada para almacenar los datos de las ventas.
+        private VentaData _ventaData;
+
+        // Constructor que recibe los datos de las ventas como parámetro.
+        public VentaController(VentaData ventaData)
+        {
+            // Verifica que los datos de las ventas no sean nulos antes de asignarlos.
+            this._ventaData = ventaData ?? throw new ArgumentNullException(nameof(ventaData));
+        }
+
+    }
+
+}
