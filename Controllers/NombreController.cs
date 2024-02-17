93445473576b50180b0214775b6ba24709f56a66
lautaro-iriazabal <lautaroiriazabal@gmail.com>
2024-02-17 15:39:58 -0300

    clase nombre controllers

---
+++ Controllers/NombreController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiLautaroIriazabal.Controllers
+{
+    // La anotación ApiController indica que esta clase es un controlador de API.
+    [ApiController]
+    // La anotación Route define la ruta base para las acciones de este controlador.
+    [Route("api/[controller]")]
+    public class NombreController : Controller
+    {
+        // Método para obtener un nombre. Se accede a través de una petición GET.
+        [HttpGet]
+        public string ObtenerNombre()
+        {
+            // Devuelve un nombre fijo.
+            return "Lautaro Iriazabal";
+        }
+
+        // Método para obtener un listado de nombres. Se accede a través de una petición GET a "api/Nombre/Listado".
+        [HttpGet("Listado")]
+        public List<String> ObtenerListadoDeNombres()
+        {
+            // Crea una lista de nombres y la devuelve.
+            List<String> list = new List<String>() { "---Listado de nombres---" };
+            return list;
+        }
+
+        // Método para obtener parámetros desde la query string de la URL. Se accede a través de una petición GET a "api/Nombre/QueryParam".
+        [HttpGet("QueryParam")]
+        public IActionResult Parametros([FromQuery] string nombre, [FromQuery] string edad)
+        {
+            // Devuelve un objeto anónimo con los parámetros recibidos.
+            return base.Ok(new { parametro = new List<string> { nombre, edad } });
+        }
+    }
+
+}
--- database/CoderContext.cs
+++ database/CoderContext.cs
@@ -1,6 +1,101 @@
-﻿namespace WebApiLautaroIriazabal.database
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace WebApiLautaroIriazabal.database
 {
-    public class CoderContext
+    // La clase CoderContext hereda de DbContext y se utiliza para interactuar con la base de datos.
+    public partial class CoderContext : DbContext
     {
+        // Constructor vacío
+        public CoderContext()
+        {
+        }
+
+        // Constructor que toma opciones de DbContext
+        public CoderContext(DbContextOptions<CoderContext> options)
+            : base(options)
+        {
+        }
+
+        // Definición de las tablas de la base de datos
+        public virtual DbSet<Usuario> Usuarios { get; set; } = null!;
+        public virtual DbSet<Producto> Productos { get; set; } = null!;
+        public virtual DbSet<Venta> Venta { get; set; } = null!;
+        public virtual DbSet<ProductoVendido> ProductoVendidos { get; set; } = null!;
+
+        // Método para configurar el modelo de la base de datos
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configuración de la entidad Usuario
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.ToTable("Usuario"); // Nombre de la tabla
+
+                // Propiedades de la entidad
+                entity.Property(e => e.Nombre).IsUnicode(false);
+                entity.Property(e => e.Apellido).IsUnicode(false);
+                entity.Property(e => e.NombreUsuario).IsUnicode(false);
+                entity.Property(e => e.Mail).IsUnicode(false);
+                entity.Property(e => e.Contraseña).IsUnicode(false);
+            });
+
+            // Configuración de la entidad Producto
+            modelBuilder.Entity<Producto>(entity =>
+            {
+                entity.ToTable("Producto"); // Nombre de la tabla
+
+                // Propiedades de la entidad
+                entity.Property(e => e.Descripciones).IsUnicode(false);
+                entity.Property(e => e.Costo).HasColumnType("money");
+                entity.Property(e => e.PrecioVenta).HasColumnType("money");
+
+                // Relación con la entidad Usuario
+                entity.HasOne(d => d.IdUsuarioNavigation)
+                    .WithMany(p => p.Productos)
+                    .HasForeignKey(d => d.IdUsuario)
+                    .OnDelete(DeleteBehavior.Cascade)
+                    .HasConstraintName("FK_Producto_Usuario");
+            });
+
+            // Configuración de la entidad Venta
+            modelBuilder.Entity<Venta>(entity =>
+            {
+                entity.Property(e => e.Comentarios).IsUnicode(false);
+
+                // Relación con la entidad Usuario
+                entity.HasOne(d => d.IdUsuarioNavigation)
+                    .WithMany(p => p.Venta)
+                    .HasForeignKey(d => d.IdUsuario)
+                    .OnDelete(DeleteBehavior.Cascade)
+                    .HasConstraintName("FK_Venta_Usuario");
+            });
+
+            // Configuración de la entidad ProductoVendido
+            modelBuilder.Entity<ProductoVendido>(entity =>
+            {
+                entity.ToTable("ProductoVendido"); // Nombre de la tabla
+
+                // Relación con la entidad Producto
+                entity.HasOne(d => d.IdProductoNavigation)
+                    .WithMany(p => p.ProductoVendidos)
+                    .HasForeignKey(d => d.IdProducto)
+                    .OnDelete(DeleteBehavior.Cascade)
+                    .HasConstraintName("FK_ProductoVendido_Producto");
+
+                // Relación con la entidad Venta
+                entity.HasOne(d => d.IdVentaNavigation)
+                    .WithMany(p => p.ProductoVendidos)
+                    .HasForeignKey(d => d.IdVenta)
+                    .OnDelete(DeleteBehavior.Cascade)
+                    .HasConstraintName("FK_ProductoVendido_Venta");
+            });
+
+            // Método parcial para configuraciones adicionales
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        // Método parcial para ser sobrescrito en caso de necesitar configuraciones adicionales
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
+
 }
