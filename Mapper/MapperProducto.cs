6d094e75e76e0f911cd884c6ce3b71300e6c5b83
lautaro-iriazabal <lautaroiriazabal@gmail.com>
2024-03-06 20:41:34 -0300

    producto mapper

---
+++ Mapper/MapperProducto.cs
@@ -0,0 +1,44 @@
+﻿using WebApiLautaroIriazabal.DTO;
+using WebApiLautaroIriazabal.Models;
+
+namespace WebApiLautaroIriazabal.Mapper
+{
+    public class MapperProducto
+    {
+        // Método para mapear un ProductoDTO a un Producto
+        public Producto MapearToProducto(ProductoDTO dto)
+        {
+            // Crea un nuevo objeto Producto
+            Producto producto = new Producto();
+
+            // Asigna los valores del DTO al Producto
+            producto.Id = dto.Id;
+            producto.Descripciones = dto.Descripciones;
+            producto.Costo = dto.Costo;
+            producto.Stock = dto.Stock;
+            producto.PrecioVenta = dto.PrecioVenta;
+            producto.IdUsuario = dto.IdUsuario;
+
+            // Retorna el Producto mapeado
+            return producto;
+        }
+
+        // Método para mapear un Producto a un ProductoDTO
+        public ProductoDTO MapearToDTO(Producto producto)
+        {
+            // Crea un nuevo objeto ProductoDTO
+            ProductoDTO dto = new ProductoDTO();
+
+            // Asigna los valores del Producto al DTO
+            dto.Id = producto.Id;
+            dto.Descripciones = producto.Descripciones;
+            dto.Costo = producto.Costo;
+            dto.Stock = producto.Stock;
+            dto.PrecioVenta = producto.PrecioVenta;
+            dto.IdUsuario = producto.IdUsuario;
+
+            // Retorna el DTO mapeado
+            return dto;
+        }
+    }
+}
