de5981d8d02ec31b73c48b6cc6b4ecf6ee8bedb7
lautaro-iriazabal <lautaroiriazabal@gmail.com>
2024-03-06 20:56:16 -0300

    usuario mapper

--- Mapper/MapperUsuario.cs
+++ Mapper/MapperUsuario.cs
@@ -1,6 +1,38 @@
-﻿namespace WebApiLautaroIriazabal.Mapper
+﻿using WebApiLautaroIriazabal.DTO;
+using WebApiLautaroIriazabal.Models;
+
+namespace WebApiLautaroIriazabal.Mapper
 {
-    public class MapperUsuario
+    public class UsuarioMapper
     {
+        public Usuario MapearToUsuario(UsuarioDTO dto)
+        {
+            Usuario usuario = new Usuario();
+            usuario.Id = dto.Id;
+            usuario.Nombre = dto.Nombre;
+            usuario.Apellido = dto.Apellido;
+            usuario.NombreUsuario = dto.NombreUsuario;
+            usuario.Contraseña = dto.Contraseña;
+            usuario.Mail = dto.Mail;
+
+            return usuario;
+
+        }
+
+        public UsuarioDTO MapearToDTO(Usuario usuario)
+        {
+            UsuarioDTO dto = new UsuarioDTO();
+            dto.Id = usuario.Id;
+            dto.Nombre = usuario.Nombre;
+            dto.Apellido = usuario.Apellido;
+            dto.NombreUsuario = usuario.NombreUsuario;
+            dto.Contraseña = usuario.Contraseña;
+            dto.Mail = usuario.Mail;
+
+            return dto;
+
+        }
+
+
     }
 }
