3584fde02dc1716a24eea7cb7deb6e9105832f23
lautaro-iriazabal <lautaroiriazabal@gmail.com>
2024-03-06 20:53:57 -0300

    Venta DTO

--- DTO/UsuarioDTO.cs
+++ DTO/UsuarioDTO.cs
@@ -2,5 +2,11 @@
 {
     public class UsuarioDTO
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Apellido { get; set; } = null!;
+        public string NombreUsuario { get; set; } = null!;
+        public string Contraseña { get; set; } = null!;
+        public string Mail { get; set; } = null!;
     }
 }
---
+++ DTO/VentaDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiLautaroIriazabal.DTO
+{
+    public class VentaDTO
+    {
+        public int Id { get; set; }
+        public string? Comentarios { get; set; }
+        public int IdUsuario { get; set; }
+
+    }
+}
