d8f249e82f4681ce077e7525189a9e49dc583db7
lautaro-iriazabal <lautaroiriazabal@gmail.com>
2024-03-06 21:11:44 -0300

    producto Vendido mapper en prd vendido data

--- Service/ProductoVendidoData.cs
+++ Service/ProductoVendidoData.cs
@@ -1,17 +1,51 @@
 ﻿using WebApiLautaroIriazabal.database;
 using WebApiLautaroIriazabal.Models;
 using WebApiLautaroIriazabal.Mapper;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore;
+using WebApiLautaroIriazabal.DTO;
 
 namespace WebApiLautaroIriazabal.Service
 {
     public class ProductoVendidoData
     {
+        // Definición de las propiedades de la clase
         private CoderContext context;
-        public ProductoVendidoData(CoderContext coderContext)
+        private MapperProductoVendido productoVendidoMapper;
+
+        // Constructor de la clase
+        public ProductoVendidoData(CoderContext coderContext, MapperProductoVendido productoVendidoMapper)
         {
             this.context = coderContext;
+            this.productoVendidoMapper = productoVendidoMapper;
         }
 
+        // Método para obtener los productos vendidos por ID de usuario
+        public List<ProductoVendidoDTO> ObtenerProductosVendidosPorIdUsuario(int idUsuario)
+        {
+            List<Producto>? productos = this.context.Productos
+                .Include(p => p.ProductoVendidos).Where(p => p.IdUsuario == idUsuario).ToList();
+
+            List<ProductoVendido?>? pVendidos = productos.Select(p => p.ProductoVendidos.ToList().
+            Find(pv => pv.IdProducto == p.Id)).Where(p => !object.ReferenceEquals(p, null)).ToList();
+
+            List<ProductoVendidoDTO> dto = pVendidos.Select(p => this.productoVendidoMapper.MapearToDTO(p)).ToList();
+
+            return dto;
+        }
+
+        // Método para agregar un producto vendido
+        public bool AgregarUnProductoVendido(ProductoVendidoDTO productoVendidoDTO)
+        {
+            ProductoVendido ProductoVendido = this.productoVendidoMapper.MapearToProdcutoVendido(productoVendidoDTO);
+            EntityEntry<ProductoVendido>? resultado = this.context.ProductoVendidos.Add(ProductoVendido);
+            resultado.State = Microsoft.EntityFrameworkCore.EntityState.Added;
+            context.SaveChanges();
+
+            return true;
+        }
+
+        // Método para obtener un producto vendido por su ID
         public ProductoVendido ObtenerProductoVendido(int id)
         {
             ProductoVendido productoBuscadoVendido = context.ProductoVendidos.Where(u => u.Id == id).FirstOrDefault();
@@ -19,6 +53,7 @@ namespace WebApiLautaroIriazabal.Service
             return productoBuscadoVendido;
         }
 
+        // Método para listar todos los productos vendidos
         public List<ProductoVendido> ListarProductosVendidos()
         {
             List<ProductoVendido> productoVendido = context.ProductoVendidos.ToList();
@@ -26,6 +61,7 @@ namespace WebApiLautaroIriazabal.Service
             return productoVendido;
         }
 
+        // Método para crear un producto vendido
         public bool CrearProductoVendido(ProductoVendido productoVendido)
         {
             context.ProductoVendidos.Add(productoVendido);
@@ -34,6 +70,7 @@ namespace WebApiLautaroIriazabal.Service
             return true;
         }
 
+        // Método para modificar un producto vendido
         public bool ModificarProductoVendido(ProductoVendido productoVendido, int id)
         {
             ProductoVendido ProductoVendidoBuscado = context.ProductoVendidos.Where(pv => pv.Id == id).FirstOrDefault();
@@ -48,6 +85,7 @@ namespace WebApiLautaroIriazabal.Service
             return true;
         }
 
+        // Método para eliminar un producto vendido
         public bool EliminarProductoVendido(int id)
         {
             ProductoVendido productoABorradoVendido = context.ProductoVendidos.Where(pv => pv.Id == id).FirstOrDefault();
@@ -62,4 +100,5 @@ namespace WebApiLautaroIriazabal.Service
             return false;
         }
     }
+
 }
