647ecded91349a982eab07145ab7d6eb04d90b44
lautaro-iriazabal <lautaroiriazabal@gmail.com>
2024-03-13 11:39:53 -0300

    cambios en program

--- Program.cs
+++ Program.cs
@@ -8,60 +8,61 @@ using WebApiLautaroIriazabal.Service;
 
 namespace WebApiLautaroIriazabal
 {
-    public class Program
-    {
-        public static void Main(string[] args)
+ 
+        public class Program
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddScoped<UsuarioData>();
-            builder.Services.AddScoped<ProductoData>();
-            builder.Services.AddScoped<ProductoVendidoData>();
-            builder.Services.AddScoped<VentaData>();
-
-
-            builder.Services.AddCors(options =>
+            public static void Main(string[] args)
             {
-                options.AddDefaultPolicy(policy => {
-                    policy.AllowAnyMethod();
-                    policy.AllowAnyOrigin();
-                    policy.AllowAnyHeader();
+                var builder = WebApplication.CreateBuilder(args);
+
+                // Add services to the container.
+
+                builder.Services.AddControllers();
+                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+                builder.Services.AddScoped<UsuarioData>();
+                builder.Services.AddScoped<ProductoData>();
+                builder.Services.AddScoped<ProductoVendidoData>();
+                builder.Services.AddScoped<VentaData>();
+
+
+                builder.Services.AddCors(options =>
+                {
+                    options.AddDefaultPolicy(policy => {
+                        policy.AllowAnyMethod();
+                        policy.AllowAnyOrigin();
+                        policy.AllowAnyHeader();
+
+                    });
 
                 });
 
-            });
+
+                builder.Services.AddDbContext<CoderContext>(option => //Agrega un contexto de tipo DataBaseContext. aui se realiza la conexion.
+                {
+                    string server = ".";
+                    string dataBase = "coderhouse";
+                    option.UseSqlServer(($"Server={server}; Database={dataBase}; Trusted_Connection=True;"));
+                });
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
 
 
-            builder.Services.AddDbContext<CoderContext>(option =>    //Agrega un contexto de tipo DataBaseContext. aca se realiza la conexion.
-            {
-                string server = ".";
-                string dataBase = "coderhouse";
-                option.UseSqlServer(($"Server={server}; Database={dataBase}; Trusted_Connection=True;"));
-            });
+                app.UseCors();
+                app.UseHttpsRedirection();
+                app.UseAuthorization();
+                app.MapControllers();
 
-            var app = builder.Build();
+                app.Run();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
             }
-
-
-            app.UseCors();
-            app.UseHttpsRedirection();
-            app.UseAuthorization();
-            app.MapControllers();
-
-            app.Run();
-
         }
     }
-}
